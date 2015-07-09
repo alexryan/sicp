c79ab5d4525885340447a63dd754944a2a9f28c8
Alex Ryan <alexander.j.ryan@gmail.com>
2015-07-09 04:51:47 -0400

    figure out how to use mit-scheme from the command-line

--- ch01/ex1-3.scm
+++ ch01/ex1-3.scm
@@ -1,9 +1,9 @@
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Exercise 1.3: Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers
 ;;;
 ;;; $mit-scheme --load ex1-3.scm 
 ;;; (q3 2 3 4) => 25
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define (square x) (* x x))
 ;;;(define (max a b) (cond ((> a b) a) (else b)) )
@@ -18,4 +18,7 @@
 ;;; (q3 4 3 2) => 25
 ;;; (q3 3 1 4) ==> 25
 (define (q3 a b c) (- (sumofsquares a b c) (square (min3 a b c)) ) )
+
+(display (q3 2 3 4))
+(display "\n")
 
---
+++ ch01/runAll.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo 'in scheme ...'
+mit-scheme -silent < ex1-3.scm 
+
+echo 'in R ...'
+RScript ex1-3.R
+
+echo 'in scala ...'
+scala ex1-3.scala 
+
